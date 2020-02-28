0ef9d00b30722c4581ee99b51f0787438faa2378
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2020-02-28 14:23:15 -0500

    update

--- TensorRT/python/quickHcalRun.sh
+++ TensorRT/python/quickHcalRun.sh
@@ -1,21 +1,20 @@
 echo "ssh-ing into $(hostname) succeeded."
 cd $1
-timetoRun=$2
+timetoRun=$(date -d "$2" +%s)
 name=$3
 timeNow=$(date +%s)
-
+echo $timetoRun
+echo $timeNow
 
 mkdir -p data/${name}
 log="data/${name}/${RANDOM}.log"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`
 
-#sleep_seconds=$(echo "$timetoRun - $timeNow"|bc)
-echo date >$log
-#sleep $sleep_seconds
-echo "Sleeping $sleep_seconds seconds" > $log 
-echo date >$log
-echo $pwd
+sleep_seconds=$(($timetoRun - $timeNow))
+echo "Sleeping $sleep_seconds seconds"  
+sleep $sleep_seconds
 cmsRun OnLine_HLT_GRun.py  ##maxEvents=25 address=t3btch042.mit.edu port=8001
 
 echo "Process on $(hostname) finished."
+
---
+++ TensorRT/python/spawn_old.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# spawn.sh
+
+# Spawns a lot of instances of cmsRun Hcal*.py on whatever machines you like
+# Use the -c flag to clear the data directory beforehand.
+
+# Example of use:
+#
+#    ./spawn.sh -c 10 jtdinsmo t3btch043.mit.edu t3desk014.mit.edu
+#
+# will clear the data directory and spawn 10 clients each on t3btch043.mit.edu 
+# and t3desk014.mit.edu. These clients will run in their own subprocess until they
+# quit, but they will continue to print output onto the terminal that you ran the 
+# ./spawn.sh from. To determine if they are still running, you can run `ps` on 
+# from the machine that you ran ./spawn.sh from, and every instance of `sshpass`
+# is a client. Or, you can run ps -e on any of the machines you listed, and every
+# instance of cmsRun will be a client running on that specific machine.
+
+# So for the above input, ps will register 20 instances of sshpass on whatever
+# machine you ran the command on, and ps -e will register 10 instances of cmsRun
+# each on t3btch043.mit.edu and t3desk014.mit.edu.
+
+
+# Determine whether the -c flag was used
+getopts 'c' flag
+clearSet=0
+
+case ${flag} in
+    c ) clearSet=1;;
+esac
+
+# Check to make sure the number of clients was given
+if [ "$#" -le "$(($clearSet+1))" ]; then
+    echo "Please provide the number of clients to spawn and your username, in that order."
+    exit
+fi
+
+numClients="$((1+$clearSet))"
+userName="$((2+$clearSet))"
+minHostNum="$((3+$clearSet))"
+hostFile="${!minHostNum}"
+name=$5
+pathToPython="~/CMSSW_10_6_6/src/SonicCMS/TensorRT/python/"
+
+# Clear the data directory if -c was used
+if [ $clearSet == 1 ]; then
+    echo "Clearing data directory"
+    rm data/*.dat
+fi
+
+# Get the ssh password
+echo "Please enter the ssh password for ${!userName}"
+read -s password
+
+# Run the clients
+while read hostNum; do
+    mkdir -p data/$name/$hostNum
+    rm -r data/$name/$hostNum/*
+    echo "Starting host ${hostNum}"
+    for ((i=0; i < ${!numClients}; i++))
+    do
+        echo "Starting client number $i at ${hostNum}"
+        log="data/$name/$hostNum/$i.log"
+        gcloud compute ssh jeffkrupa@${hostNum} --zone us-central1-a --command="sh $pathToPython/quickHcalRun.sh $pathToPython '19:22:00' $name" &> $log &
+
+    done
+    disown
+done <$hostFile
+
+
+echo "Done"
