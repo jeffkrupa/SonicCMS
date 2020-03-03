77b065a2c8b57e6ff02f3e9c1680544efb1d58d0
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2020-03-03 12:08:16 -0500

    update

--- TensorRT/python/spawn_old.sh
+++ TensorRT/python/spawn_old.sh
@@ -34,7 +34,8 @@ while read hostNum; do
     rm -r data/$name/$hostNum/*
     echo "Starting host ${hostNum}"
 
-    gcloud compute ssh jeffkrupa@${hostNum} --zone us-central1-a --command="cd $pathToPython; git pull -f; cd ../..; source /cvmfs/cms.cern.ch/cmsset_default.sh; cmsenv; scram b -j 8; cd -" 
+    gcloud compute ssh jeffkrupa@${hostNum} --zone "us-central1-a" --command="cd $pathToPython; git pull -f; cd ../..; source /cvmfs/cms.cern.ch/cmsset_default.sh; cmsenv; scram b -j 8; cd -;" 
+
     for ((i=0; i < ${!numClients}; i++))
     do
         pkill -USR1 cmsRun
