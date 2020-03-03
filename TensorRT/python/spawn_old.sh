ab1fe03703dc764e1658c758dad179e28b81bec6
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2020-03-03 11:58:23 -0500

    update

--- TensorRT/python/spawn_old.sh
+++ TensorRT/python/spawn_old.sh
@@ -41,13 +41,13 @@ while read hostNum; do
         pkill -USR1 cmsRun
         echo "Starting client number $i at ${hostNum}"
         log="data/$name/$hostNum/$i.log"
-        gcloud compute ssh jeffkrupa@${hostNum} --zone us-central1-a\
-	--command="cd $pathToPython;\
-	 rm DQM.root;
-	 sh $pathToPython/quickHcalRun.sh $pathToPython $timetorun $name" &> $log &
+        #gcloud compute ssh jeffkrupa@${hostNum} --zone us-central1-a\
+	#--command="cd $pathToPython;\
+	# rm DQM.root;
+	# sh $pathToPython/quickHcalRun.sh $pathToPython $timetorun $name" &> $log &
 
     done
-    #disown
+    disown
 
 done <$hostFile
 
