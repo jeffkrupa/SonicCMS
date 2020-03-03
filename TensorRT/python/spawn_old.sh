87dd5551e67df4122d3b528bc3e9999551d00819
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2020-03-03 12:02:55 -0500

    update

--- TensorRT/python/spawn_old.sh
+++ TensorRT/python/spawn_old.sh
@@ -34,7 +34,7 @@ while read hostNum; do
     rm -r data/$name/$hostNum/*
     echo "Starting host ${hostNum}"
 
-    gcloud alpha compute ssh jeffkrupa@${hostNum} --zone us-central1-a --command="cd $pathToPython; git pull -f; cd ../..; source /cvmfs/cms.cern.ch/cmsset_default.sh; cmsenv; scram b -j 8; cd -"
+    gcloud alpha compute ssh jeffkrupa@${hostNum} --zone us-central1-a --command="cd $pathToPython; git pull -f; cd ../..; source /cvmfs/cms.cern.ch/cmsset_default.sh; cmsenv; scram b -j 8; cd -" --ssh-key-file=~/.ssh/gcloud
 
     for ((i=0; i < ${!numClients}; i++))
     do
