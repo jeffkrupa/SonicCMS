982ea8fa42bcd1963afd80257b2e13fb7c6713ac
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2020-03-04 20:09:32 -0500

    update

--- TensorRT/python/quickHcalRun.sh
+++ TensorRT/python/quickHcalRun.sh
@@ -19,8 +19,9 @@ sleep $sleep_seconds
 echo "EXECUTING cmsRun $4 at $date"
 cmsRun $4 #OnLine_HLT_GRun_nominal_2.py
 
-mkdir data/$name/$hostname
-cp DQM* data/$name/$hostname
+mkdir data/$name/$(hostname)
+rm data/$name/$(hostname)/*
+cp DQM* data/$name/$(hostname)
 
 #cmsRun HcalTest_mc_cfg.py maxEvents=10000 address=104.198.183.53 inputfile=TRK-Run3Summer19DR-00017_step1_test.root threads=4 ##maxEvents=25 address=t3btch042.mit.edu port=8001
 echo "HCALTEST!"
